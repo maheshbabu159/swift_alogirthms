31248663150283b93249d8304f6f9bfea3e81439
Mahesh <maheshjabu.somineni@hal.hitachi.com>
2020-02-18 22:03:45 -0800

    Miss

---
+++ Problems.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//let topCompetitors = TopCompetitors()
+//let reviews = ["Hello how are you amazon", "amazon is awesome", "facebook is good"]
+//let competitors = ["facebook", "amazon"]
+//let result = topCompetitors.getTopCompetitors(reviews: reviews, competitors: competitors, noOfTopCompetitors: 1)
+//print(result.joined(separator: ","))
+
+ 
+//let distributeFile = DistributeFile()
+//let servers = [[1, 0, 0, 0, 0,0], [1, 0, 0, 0, 0,0], [1, 0, 0, 0, 0,0], [1, 0, 0, 0,0,0], [1, 0, 0, 0,0,0]]
+//let result = distributeFile.getHoursRequiredToDistribute(servers: servers)
+//print("Hours = \(result)")
